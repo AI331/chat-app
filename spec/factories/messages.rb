8064508f68d8b3f6de6ff6383ba22d174d2bca66
AI331 <akira0331ai@gmail.com>
2021-07-05 18:00:43 +0900

    結合テストの実装

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -5,8 +5,7 @@ FactoryBot.define do
     association :room
 
     after(:build) do |message|
-      message.image.attach(io: File.open('public/images/シマエナガ.jpg_large'), filename: 'test_image.png')
+      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
-
   end
 end
